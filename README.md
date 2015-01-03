fa630eed3c418485c12360ccabc8a298033abdd1
Hanmoi Choi <daniel@myadbox.coma.au>
2015-01-03 13:40:54 +1100

    init commit

---
+++ README.md
@@ -0,0 +1,50 @@
+clojure-operator
+====
+This project is created with `lein new dacom` and modified in directory structure and libs' version
+A web application built with the DACOM stack.
+
+## Usage
+### Run datomic
+`sudo datomic-transactor  ~/Development/clojure-operator/config/free-transactor.properties`
+`lein install-db`
+
+### Install JS libraries with `bower`
+`bower install`
+
+### Run server with compojure
+`lein run-server`
+
+### Run client with om
+`lein run-client`
+
+### Debug, Hack, and Play!
+
+- Run lein cljsbuild auto dev to monitor .cljs files for changes and automatically recompile them
+
+- Run lein watch-less to monitor less for changes and recompile
+
+- Run lein repl to start the usual Leiningen/nREPL-based REPL
+Then, if you want to, type (browser-repl) or just (br) into the REPL to start an Austin-based browser REPL session. Once you've done that, refresh the webapp and try typing (js/alert "Hey there!") from the Clojure REPL
+
+## Production-Style Build
+
+- Just run lein dist in the project directory
+- Then "deploy" like:
+
+`$ cd dist/server`
+`$ echo "{:datomic-uri \"<some-datomic-uri>\" :port 3000}" > config.edn`
+`$ java -jar <project-name>-<version-number>-standalone.jar`
+`$ Started server on port 3000`
+
+- Then, for example:
+`$ cd <project-root>/dist/static && python -m SimpleHTTPServer`
+`$ Serving HTTP on 0.0.0.0 port 8000 ...`
+
+- The app will be available on http://localhost:8000
+
+## License
+
+Copyright © 2015 FIXME
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,90 @@
+;   Copyright (c) 2014 Kevin Bell. All rights reserved.
+;   See the file license.txt for copying permission.
+
+(defproject clojure-operator "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 ;; WEB
+                 [ring/ring-core "1.3.2"]
+                 [ring/ring-json "0.3.1"]
+                 [ring-cors "0.1.6"]
+                 [ring-middleware-format "0.4.0"]
+                 [compojure "1.3.1"]
+                 ;; Async
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 ;; Clojure Script
+                 [org.clojure/clojurescript "0.0-2511"]
+                 [cljs-ajax "0.3.3"]
+                 [cljs-http "0.1.23"]
+
+                 ;;React
+                 [om "0.8.0-beta5"]
+                 ;;Cljs REPL
+                 [com.cemerick/austin "0.1.5"]
+                 ;; DB
+                 [com.datomic/datomic-free "0.9.5067"]
+                 ]
+
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [lein-ring "0.8.13"]
+            [lein-pdo "0.1.1"]
+            [lein-figwheel "0.2.0-SNAPSHOT"]
+            [lein-resource "0.3.1"]
+            [lein-httpd "1.0.0"]
+            [lein-shell "0.3.0"]
+            [fsrun "0.1.2"]]
+
+
+  :source-paths ["src"]
+  :target-path "target/"
+  :uberjar-exclusions [#".*\.cljs"]
+  :cljsbuild {:builds {:dev {:source-paths ["utils/src" "src/cljs/com/myadbox"]
+                             :compiler {:output-to "static/js/main.js"
+                                        :output-dir "static/js"
+                                        :optimizations :none
+                                        :pretty-print true
+                                        :source-map true}}
+                       :prod {:source-paths ["src"]
+                              :compiler {:output-to "dist/static/js/main.js"
+                                         :optimizations :advanced
+                                         :pretty-print false
+                                         ;; From Om jar
+                                         :preamble ["react/react.min.js"]
+                                         :externs ["react/externs/react.js"]}}}}
+  :ring {:init clojure-operator.server/init-conn
+         :handler clojure-operator.server/app}
+  :profiles {:dev {
+                    ;; This needs to be here because of https://github.com/cemerick/austin/issues/23
+                    :plugins [[com.cemerick/austin "0.1.5"]]
+                    :source-paths ["utils/src"]
+                    :repl-options {:init-ns clojure-operator.repl}
+                    :resource {:resource-paths ["web-resources/pages"]
+                               :target-path "static"
+                               :extra-values {:scripts [{:src "../bower_components/react/react.js"}
+                                                        {:src "js/goog/base.js"}
+                                                        {:src "js/main.js"}
+                                                        {:body "goog.require('clojure_operator.client')"}
+                                                        {:body "goog.require('clojure_operator.repl')"}]}}}
+             :db [:dev {:main clojure-operator.db}]
+             :prod {:main clojure-operator.server
+                    :target-path "dist/server/"
+                    :resource {:resource-paths ["web-resources/pages"]
+                               :target-path "dist/static"
+                               :extra-values {:scripts [{:src "js/main.js"}]}}}
+             :uberjar {:omit-source true
+                       :aot :all}}
+  :aliases {"bower" ["shell" "bower" "install"]
+            "less-debug" ["shell" "lessc" "web-resources/stylesheets/style.less" "static/css/style.css"
+                          "--include-path=bower_components/bootstrap/less/" "--source-map"]
+            "less-prod" ["shell" "lessc" "web-resources/stylesheets/style.less" "dist/static/css/style.css"
+                         "--include-path=bower_components/bootstrap/less/" "--compress"]
+            "watch-less" ["fschange" "web-resources/stylesheets/*" "less-debug"]
+            "install-db" ["with-profile" "db" "run"]
+            "run-client" ["do" "bower," "cljsbuild" "once" "dev," "less-debug," "resource," "httpd" "8000"]
+            "run-server" ["ring" "server-headless"]
+            "dist" ["with-profile" "prod" "do" "bower," "uberjar," "cljsbuild" "once" "prod," "less-prod,"
+                    "resource"]}
+  :clean-targets [:target-path :compile-path "static" "dist"])
