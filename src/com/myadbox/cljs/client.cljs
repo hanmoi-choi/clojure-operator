1036adb7a5bb16bd01db3d509da49c732101295b
Hanmoi Choi <daniel@myadbox.coma.au>
2015-01-03 18:30:16 +1100

    first om/component

--- src/com/myadbox/cljs/client.cljs
+++ src/com/myadbox/cljs/client.cljs
@@ -12,7 +12,44 @@
           {:handler (fn [response]
                       (put! server-chan response))})
 
-(def app-state (atom {:list ["Lion" "Zebra" "Buffalo" "Antelope"]}))
+(def app-state
+  (atom
+    {:contacts
+     [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+      {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+      {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+      {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+      {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+      {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
+
+(defn middle-name [{:keys [middle middle-initial]}]
+  (cond
+    middle (str " " middle)
+    middle-initial (str " " middle-initial ".")))
+
+(defn display-name [{:keys [first last] :as contact}]
+  (str last ", " first (middle-name contact)))
+
+(defn contact-view [contact owner]
+  (reify
+    om/IRender
+    (render [_]
+      (html/html
+        [:li (display-name contact)])
+      #_(dom/li nil (display-name contact)))))
+
+(defn contacts-view [app owner]
+  (reify
+    om/IRender
+    (render [this]
+      (html/html
+        [:div
+         [:h2 "Contact list"]
+         [:ul (om/build-all contact-view (:contacts app))]])
+      #_(dom/div nil
+               (dom/h2 nil "Contact list")
+               (apply dom/ul nil
+                      (om/build-all contact-view (:contacts app)))))))
 
 (defn draw-list [app owner]
   (reify
@@ -39,6 +76,6 @@
 
 
 (om/root
-  draw-list
+  contacts-view
   app-state
   {:target (. js/document (getElementById "app"))})
