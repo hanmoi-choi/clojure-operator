bc06c23241f0e2b2617ef13a0606bdf33b292568
Hanmoi Choi <daniel@myadbox.coma.au>
2015-01-05 23:03:59 +1100

    clean up codes

--- src/com/myadbox/clj/components/imageconverter/image_magic_wrapper.clj
+++ src/com/myadbox/clj/components/imageconverter/image_magic_wrapper.clj
@@ -3,40 +3,26 @@
 
 (def allowed-convert-args [:density :resize :src :dst])
 
-(defn imagemagic-convert
-  #_{:pre (every? #(contains? opts %) allowed-convert-args)}                         ;;only allowed args
-  [opts]
-  (let [convert-args (to-shell-args opts)]
-    (apply sh/sh "convert" convert-args)))
+(defn to-shell-args [args]
+  (let [opts (dissoc test-arg :src :dst)
+        convert-opts (mapv (fn [[k v]] [(str "-" (name k)) v]) opts)
+        src (:src args)
+        dst (:dst args) ]
+    (flatten (conj convert-opts src dst))))
 
-(imagemagic-convert test-arg)
 (def test-arg {:density "400"
                :resize "25%"
                :src "/Users/daniel/Downloads/cheatsheet-usletter-color.pdf"
                :dst "/Users/daniel/Downloads/image.jpg"
                })
 
-#_(defn get-opts [args]
-  (let [opts ()]))
+(defn imagemagic-convert
+  [opts]
+  {:pre (every? #(contains? opts %) allowed-convert-args)}                         ;;only allowed args
+  (let [convert-args (to-shell-args opts)]
+    (apply sh/sh "convert" convert-args)))
 
-(defn to-shell-args [args]
-  (let [opts (dissoc test-arg :src :dst)
-        convert-opts (mapv (fn [[k v]] [(str "-" (name k)) v]) opts)
-        src (:src args)
-        dst (:dst args) ]
-     (flatten (conj convert-opts src dst))))
-
-(to-shell-args test-arg)
-
-(:src test-arg)
-
-(dissoc test-arg :src :dst)
-(map #(name %) (keys
-                 {:density "400"
-                  :resize  "25%"
-                  :src "/Users/daniel/Downloads/cheatsheet-usletter-color.pdf"
-                  :dst "/Users/daniel/Downloads/image.jpg"}))
-
+(imagemagic-convert test-arg)
 
 
 
