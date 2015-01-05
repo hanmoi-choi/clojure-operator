56895dca07f91bb21875d16c56233cd837838307
Hanmoi Choi <daniel@myadbox.coma.au>
2015-01-05 23:01:14 +1100

    working in convert

--- src/com/myadbox/clj/components/imageconverter/image_magic_wrapper.clj
+++ src/com/myadbox/clj/components/imageconverter/image_magic_wrapper.clj
@@ -1,26 +1,42 @@
 (ns com.myadbox.clj.components.imageconverter.image-magic-wrapper1
   (:require [clojure.java.shell :only [sh] :as sh :reload true]))
 
-(def allowed-convert-args [:density :resize :src :dest])
+(def allowed-convert-args [:density :resize :src :dst])
 
 (defn imagemagic-convert
-  {:pre (every? #(contains? opts %) allowed-convert-args)}                         ;;only allowed args
+  #_{:pre (every? #(contains? opts %) allowed-convert-args)}                         ;;only allowed args
   [opts]
-  (letfn [(convert [&args] (sh/sh "convert" args))]
-    (apply convert opts src dest)))
+  (let [convert-args (to-shell-args opts)]
+    (apply sh/sh "convert" convert-args)))
 
-(imagemagic-convert
-  {:density "400"
-   :resize "25%"
-   :src "/Users/daniel/Downloads/cheatsheet-usletter-color.pdf"
-   :dest "/Users/daniel/Downloads/image.jpg"
-   })
+(imagemagic-convert test-arg)
+(def test-arg {:density "400"
+               :resize "25%"
+               :src "/Users/daniel/Downloads/cheatsheet-usletter-color.pdf"
+               :dst "/Users/daniel/Downloads/image.jpg"
+               })
 
+#_(defn get-opts [args]
+  (let [opts ()]))
+
+(defn to-shell-args [args]
+  (let [opts (dissoc test-arg :src :dst)
+        convert-opts (mapv (fn [[k v]] [(str "-" (name k)) v]) opts)
+        src (:src args)
+        dst (:dst args) ]
+     (flatten (conj convert-opts src dst))))
+
+(to-shell-args test-arg)
+
+(:src test-arg)
+
+(dissoc test-arg :src :dst)
 (map #(name %) (keys
                  {:density "400"
                   :resize  "25%"
                   :src "/Users/daniel/Downloads/cheatsheet-usletter-color.pdf"
-                  :dest "/Users/daniel/Downloads/image.jpg"}))
+                  :dst "/Users/daniel/Downloads/image.jpg"}))
+
 
 
 
