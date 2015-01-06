4e21cf3998b1d174f01c73d3a1ed3b3516bad73a
Hanmoi Choi <forhim185@gmail.com>
2015-01-07 00:32:08 +1100

    working in download and convert by using Tim pipeline

--- src/com/myadbox/clj/components/imageconverter/image_magic_wrapper.clj
+++ src/com/myadbox/clj/components/imageconverter/image_magic_wrapper.clj
@@ -1,28 +1,21 @@
-(ns com.myadbox.clj.components.imageconverter.image-magic-wrapper1
+(ns com.myadbox.clj.components.imageconverter.image-magic-wrapper
   (:require [clojure.java.shell :only [sh] :as sh :reload true]))
 
 (def allowed-convert-args [:density :resize :src :dst])
 
 (defn to-shell-args [args]
-  (let [opts (dissoc test-arg :src :dst)
+  (let [opts (dissoc args :src :dst)
         convert-opts (mapv (fn [[k v]] [(str "-" (name k)) v]) opts)
         src (:src args)
         dst (:dst args) ]
     (flatten (conj convert-opts src dst))))
 
-(def test-arg {:density "400"
-               :resize "25%"
-               :src "/Users/daniel/Downloads/cheatsheet-usletter-color.pdf"
-               :dst "/Users/daniel/Downloads/image.jpg"
-               })
 
 (defn imagemagic-convert
   [opts]
   {:pre (every? #(contains? opts %) allowed-convert-args)}                         ;;only allowed args
   (let [convert-args (to-shell-args opts)]
-    (apply sh/sh "convert" convert-args)))
-
-(imagemagic-convert test-arg)
-
+    (apply sh/sh "convert" convert-args)
+    (println "finished converting!!")))
 
 
--- src/com/myadbox/clj/components/s3/downloader.clj
+++ src/com/myadbox/clj/components/s3/downloader.clj
@@ -1,33 +1,89 @@
 (ns com.myadbox.clj.components.s3.downloader
   (:require [com.myadbox.clj.util :as myadbox-util]
+            [com.myadbox.clj.components.imageconverter.image-magic-wrapper :as converter]
             [clojure.java.io :as io]
-            [aws.sdk.s3 :as s3]))
+            [clojure.core.async :as async :refer [<!! >!! >! <! put! go]]
+            [aws.sdk.s3 :as s3])
+  (:import (javax.imageio ImageIO)))
+
+(def test-arg {:density "400"
+               :resize "25%"
+               :src "/Users/daniel/Downloads/cheatsheet-usletter-color.pdf"
+               :dst "/Users/daniel/Downloads/image.jpg"
+               })
+
+(defn to-proc< [in]
+  (let [out (async/chan 1)]
+    (async/pipe in out)
+    out))
+
+;; These code are from Tim's screencast - https://tbaldridge.pivotshare.com/
+(defn pipeline<
+  "docstring"
+  [desc c]
+  (let [p (partition 2 desc)]
+    (reduce
+      (fn [prev-c [f n]]
+        (-> (for [_ (range n)]
+              (->
+                (async/map< f prev-c)
+                to-proc<))
+            (async/merge)))
+      c
+      p))
+  )
 
 ;; create your own s3.edn
 (def s3-config (myadbox-util/load-config "config/s3.edn"))
 (def s3-cred (:cred s3-config))
 (def s3-bucket (:bucket s3-config))
 
-(s3/bucket-exists? s3-cred s3-bucket)
-
-(def obj-key "profiles/656/small")
-
-(println (slurp (:content (s3/get-object s3-cred s3-bucket obj-key))))
-(s3/get-object-metadata s3-cred s3-bucket obj-key)
-
+(def obj-key "ads/29889/cFAiWrRiM5MFBL47p.jpg")
 
 (def root-dir "/Users/daniel/Downloads/")
 (defn copy-stream [in path]
   (with-open [out (io/output-stream (io/file path))]
     (io/copy in out)))
 
+(def count (atom 0))
+(def test-arg {:density "400"
+               :resize "200%"
+               })
 
-(defn download-file-from-s3 [key]
-  (let [file-ext (:content-type (s3/get-object-metadata s3-cred s3-bucket key))
-        path (str root-dir "profile" ".jpg")
-        s3-obj-instream (:content (s3/get-object s3-cred s3-bucket obj-key))]
+(defn download-file [key]
+  (let [s3-obj (s3/get-object s3-cred s3-bucket key)
+        src-path (str root-dir (format "source-%s.jpeg" (swap! count inc)))
+        dst-path (str root-dir (format "dst-%s.jpeg" @count))
+        s3-obj-instream (:content s3-obj)]
     ;;(println (class s3-obj-instream))   ;; com.amazonaws.services.s3.model.S3ObjectInputStream
-    (copy-stream s3-obj-instream path)))  ;; s3/get-object's :cotent is objectInputStream
+    (copy-stream s3-obj-instream src-path)
+    (println "Finished downloading")
+    (assoc test-arg :src src-path :dst dst-path)))
 
+(def c (async/chan 20))
+(for [i (range 15)]
+  (go
+    (>! c obj-key)
+    (<! (pipeline< [download-file 10
+                 converter/imagemagic-convert 10] c))))
 
-(download-file-from-s3 obj-key)
+;(def images
+;  (take 10
+;        (repeatedly
+;          #(ImageIO/read (clojure.java.io/input-stream
+;                           "/Users/blake/Pictures/blake_signature.jpg")))))
+;
+;(defn save-image [image filename]
+;  (with-open [os (io/ByteArrayOutputStream.)]
+;    (ImageIO/write image "jpg" os)
+;    (let [request
+;          (s3/put-object credentials bucket-name filename
+;                         (io/input-stream (.toByteArray os)))]
+;      {:finished true :request request})))
+;
+;(defn upload-images [images]
+;  (doall
+;    (map-indexed
+;      (fn [image i]
+;        (future (upload-image image (format "myimage-%s.jpg" i))))
+;      images)))
